ebc5b6102699c923320d92a2cf73b96776637c0e
Phantomvv1 <krisi_andreev@abv.bg>
2025-02-07 10:28:13 +0200

    My config

---
+++ lua/custom/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true, -- Enable treesitter integration for smarter pairing
+			})
+		end,
+	},
+}
