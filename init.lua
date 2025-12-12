5e5c865b7e0e69a7777b518ba78b0141477fa050
Phantomvv1 <krisi_andreev@abv.bg>
2025-12-12 12:29:28 +0200

    Forgot to commit that

--- init.lua
+++ init.lua
@@ -743,8 +743,9 @@ require("lazy").setup({
 	},
 	{
 		"L3MON4D3/LuaSnip",
+		lazy = false,
 		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").load()
 		end,
 	},
 
